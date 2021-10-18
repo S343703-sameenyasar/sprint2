c9545fdd3e045252a14380cc6bcc9e55563fff8c
S343703-sameenyasar <91610040+S343703-sameenyasar@users.noreply.github.com>
2021-10-19 01:03:31 +0930

    Add files via upload

--- assignment.sql
+++ assignment.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 18, 2021 at 07:59 AM
+-- Generation Time: Oct 18, 2021 at 07:47 AM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.8
 
