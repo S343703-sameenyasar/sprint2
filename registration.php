a2995d9ef713b8fb6651a114ea3f96e80169e564
S343703-sameenyasar <91610040+S343703-sameenyasar@users.noreply.github.com>
2021-10-19 01:19:52 +0930

    for new users to registrate

---
+++ registration.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <form method = "POST" action = "create.php" >
+            
+            <p>Username</p>
+            <input type = "text" name = "username">
+            <p>Password</p>
+            <input type = "password" name = "password">
+            <input type = "submit" value = "Create Account">
+        </form>
+    </body>
+</html>
