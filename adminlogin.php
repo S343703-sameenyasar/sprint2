3d40507103548483b2d4dd8b750e7ed807dd3f3a
S343703-sameenyasar <91610040+S343703-sameenyasar@users.noreply.github.com>
2021-10-18 16:19:58 +0930

    to check if the logged in person is an admin

---
+++ adminlogin.php
@@ -0,0 +1,13 @@
+<html>
+    <body>
+        Logged in as admin.
+        <form action = "GET" action = "">
+            <button>Add a new question</button>
+        </form>
+
+        <form action = "GET" action = "deletequestion.php">
+            <button>Delete a question</button>
+        </form>
+
+    </body>
+</html>
