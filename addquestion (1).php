dd0b8d85101b796d0157e0f91de23d147d9143fe
S343703-sameenyasar <91610040+S343703-sameenyasar@users.noreply.github.com>
2021-10-19 01:04:31 +0930

    if admin wants to add questions.

---
+++ addquestion (1).php
@@ -0,0 +1,11 @@
+<?php 
+    include 'db.php'
+?>
+
+<html>
+    <body>
+        <p>Add Question Description</p>
+        <textarea rows = "5" columns = "30" name ="question_description">
+        <p>
+    </body>
+</html>
